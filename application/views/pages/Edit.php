551cec1b9026c3045116523a5ca2afbcb627ad35
ryanabi-RA <findlink09@gmail.com>
2022-12-07 23:37:30 +0700

    update minor : page responsive (profile, detail, edit)

--- application/views/pages/Edit.php
+++ application/views/pages/Edit.php
@@ -2,7 +2,7 @@
 <div class="grow m-0 py-4 w-full flex flex-wrap" id="content">
     <div class="p-4 w-full bg-white dark:bg-gray-800 rounded-xl shadow-md">
         <?php echo form_open_multipart('pegawai/update/'); ?>
-        <div class="flex flex-col lg:flex-row max-w-3xl items-center lg:items-start">
+        <div class="flex flex-col lg:flex-row max-w-4xl items-center lg:items-start">
             <img src="<?= base_url('assets/foto/') . $pegawai->foto; ?>" alt="" class="mb-6 lg:mb-0 w-fit sm:max-w-sm border-2 border-gray-600 rounded-xl bg-cover">
             <!-- </div> -->
             <table class="items-center ml-0 lg:ml-6 w-full">
@@ -52,7 +52,7 @@
             </table>
         </div>
 
-        <div class="max-w-3xl flex justify-end">
+        <div class="max-w-4xl flex justify-end">
             <div class="py-2 md:py-4 flex">
                 <button type="submit" class="mx-4 py-2 px-4 bg-blue-500 rounded-xl text-white font-bold focus:ring-2 focus:outline-none focus:ring-blue-300 border-2 border-blue-500 hover:text-white hover:bg-blue-700">Simpan</button>
                 <button type="reset" class="py-2 px-4 border-2 border-red-500 rounded-xl text-red-500 focus:ring-2 focus:outline-none focus:ring-red-300 hover:text-white hover:bg-red-500">Batal</button>
